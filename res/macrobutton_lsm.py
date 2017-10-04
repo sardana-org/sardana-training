45c5e6d74ebcf75121043495d4de835d622719d6
mrosanes <mrosanes@cells.es>
2017-10-04 16:51:43 +0200

    Reorganize and add resources

---
+++ res/macrobutton_lsm.py
@@ -0,0 +1,22 @@
+import sys
+from sardana.taurus.qt.qtgui.extra_macroexecutor import MacroButton
+from taurus.qt.qtgui.application import TaurusApplication
+
+
+
+
+if __name__ == "__main__":
+    
+    # INDICATE THE DOOR
+    door = "door_demo1_1" 
+    # INDICATE THE MACRO TO BE EXECUTED
+    macro = "lsm"
+
+    app = TaurusApplication(app_name="Widget")
+    mb = MacroButton()
+    mb.setModel(door)
+    mb.setMacroName(macro)
+    mb.setText(macro)
+    mb.show()
+    sys.exit(app.exec_())
+    
