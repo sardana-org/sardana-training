fac5c1528dfff728fff7bbed915df9f4233899b8
zreszela <zreszela@cells.es>
2019-08-08 10:41:07 +0200

    Adapt tests to py3 and create Simu class

--- blender_slits/play_slits.py
+++ blender_slits/play_slits.py
@@ -20,77 +20,89 @@ along with Sardana-Training.  If not, see <http://www.gnu.org/licenses/>.
 import socket
 import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 9999)
-sock.connect(server_address)
 
-def ask(cmd):
-    sock.sendall(cmd+'\n')
-    ans = sock.recv(4096)
-    print cmd,'->',ans[:-1]
-    return ans
+class Simu:
 
-print("This scripts documents the hardware motor controller commands")
+    def __init__(self, host='localhost', port=9999):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (host, port)
+        self._sock.connect(server_address)
 
-def waitmove():
-    ans = ask('?states')
-    while 'MOVING' in ans:
-        time.sleep(.5)
-        ans = ask('?states')
+    def __del__(self):
+        self._sock.close()
 
-print('\n\n# QUERY POSITION WITH ?pos <MOTOR>')
-ask('?pos top')
-ask('?pos bot')
-ask('?pos left')
-ask('?pos right')
+    def ask(self, cmd):
+        self._sock.sendall(cmd+b'\n')
+        ans = self._sock.recv(4096)
+        print(cmd, b'->', ans[:-1])
+        return ans
 
-print('\n\n# QUERY POSITIONS WITH ?positions')
-ask('?positions')
+    def waitmove(self):
+        ans = self.ask(b'?states')
+        while b'MOVING' in ans:
+            time.sleep(.5)
+            ans = self.ask(b'?states')
 
-print('\n\n# QUERY STATE WITH ?state <MOTOR>')
-ask('?state top')
-ask('?state bot')
-ask('?state left')
-ask('?state right')
 
-print('\n\n# QUERY STATES WITH ?states')
-ask('?states')
+if __name__ == '__main__':
+    print("This scripts documents the hardware motor controller commands")
+    s = Simu()
+    print('\n\n# QUERY POSITION WITH ?pos <MOTOR>')
+    s.ask(b'?pos top')
+    s.ask(b'?pos bot')
+    s.ask(b'?pos left')
+    s.ask(b'?pos right')
 
-print('\n\n# MOVE BLADES BY ISSUING <MOTOR> <POS>')
-ask('top 50')
-ask('bot -50')
-ask('left -50')
-ask('right 50')
-waitmove()
+    print('\n\n# QUERY POSITIONS WITH ?positions')
+    s.ask(b'?positions')
 
-print('\n\n# MOVE MULTIPLE BY ISSUING move <MOTOR1> <POS1> <MOTOR2> <POS2> ...')
-ask('move right 20 top 20 bot -20 left -20')
-waitmove()
+    print('\n\n# QUERY STATE WITH ?state <MOTOR>')
+    s.ask(b'?state top')
+    s.ask(b'?state bot')
+    s.ask(b'?state left')
+    s.ask(b'?state right')
 
-print('\n\n# CHANGE VELOCITY WITH: vel <MOTOR> <VALUE>')
-ask('vel top 1000')
-ask('vel bot 500')
-ask('vel left 50')
-ask('vel right 10')
+    print('\n\n# QUERY STATES WITH ?states')
+    s.ask(b'?states')
 
-print('\n\n# CHANGE ACCELERATION TIME WITH: acc <MOTOR> <VALUE>')
-ask('acc top 1')
-ask('acc bot 2')
-ask('acc left 3')
-ask('acc right 4')
-ask('move right 50 top 50 bot -50 left -50')
-waitmove()
+    print('\n\n# MOVE BLADES BY ISSUING <MOTOR> <POS>')
+    s.ask(b'top 50')
+    s.ask(b'bot -50')
+    s.ask(b'left -50')
+    s.ask(b'right 50')
+    s.waitmove()
+    s.ask(b'?acq_im_file')
+    s.ask(b'acq')
+    s.ask(b'?acq_im_file')
 
-print('\n\n# ABORT _ANY_ MOTION WITH abort')
-ask('move right 20 top 20 bot -20 left -20')
-for i in range(10):
-    time.sleep(.1)
-ask('abort')
-ask('?positions')
-ask('move right 20 top 20 bot -20 left -20')
-waitmove()
-ask('?positions')
-ask('?states')
+    print('\n\n# MOVE MULTIPLE BY ISSUING move <MOTOR1> <POS1> <MOTOR2> <POS2> ...')
+    s.ask(b'move right 20 top 20 bot -20 left -20')
+    s.waitmove()
 
-print('\n\n# CLOSE CONNECTION WITH q')
-ask('q')
+    print('\n\n# CHANGE VELOCITY WITH: vel <MOTOR> <VALUE>')
+    s.ask(b'vel top 1000')
+    s.ask(b'vel bot 500')
+    s.ask(b'vel left 50')
+    s.ask(b'vel right 10')
+
+    print('\n\n# CHANGE ACCELERATION TIME WITH: acc <MOTOR> <VALUE>')
+    s.ask(b'acc top 1')
+    s.ask(b'acc bot 2')
+    s.ask(b'acc left 3')
+    s.ask(b'acc right 4')
+    s.ask(b'move right 50 top 50 bot -50 left -50')
+    s.waitmove()
+
+    print('\n\n# ABORT _ANY_ MOTION WITH abort')
+    s.ask(b'move right 20 top 20 bot -20 left -20')
+    for i in range(10):
+        time.sleep(.1)
+    s.ask(b'abort')
+    s.ask(b'?positions')
+    s.ask(b'move right 20 top 20 bot -20 left -20')
+    s.waitmove()
+    s.ask(b'?positions')
+    s.ask(b'?states')
+
+    print('\n\n# CLOSE CONNECTION WITH q')
+    s.ask(b'q')
