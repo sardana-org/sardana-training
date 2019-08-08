d39c607181d59bd5ea6eb3b654d2b1d1f35e579d
zreszela <zreszela@cells.es>
2019-08-08 10:37:36 +0200

    Add camera and save rendered image (raw + png)
    Implementation done by Guifre Cuni. Simply command to the server: "acq"
    to acquire an image. To ask where the image was stored ask: "?acq_im_file".

--- blender_slits/slits_main.py
+++ blender_slits/slits_main.py
@@ -16,23 +16,35 @@ You should have received a copy of the GNU Lesser General Public License
 along with Sardana-Training.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 import bge
 import time
 import socket
+import numpy as np
+from PIL import Image
+
 from threading import Thread
 import math
 import mathutils
 from Motion import Motion
 
-#print('hello')
+# print('hello')
 scene = bge.logic.getCurrentScene()
 top = scene.objects['b_top']
 bot = scene.objects['b_bot']
 left = scene.objects['b_left']
 right = scene.objects['b_right']
 cam = scene.objects['Camera']
-#print('objects ok')
+det = scene.objects['Detector']
+det['im_number'] = 0
+det['im_file'] = 'No_file.brw'
+det['width'] = None
+det['height'] = None
+det['im_array'] = None
+# det_vpw = int(bge.render.getWindowWidth()/4)
+# det_vph = int(bge.render.getWindowHeight()/4)
+# det.setViewport(0, 0, det_vph, det_vpw)
+# det.useViewport = True
+# print('objects ok')
 
 
 # WELL-ALIGNED (GAP 0) POSITIONS ARE:
@@ -59,10 +71,11 @@ for m in motors.values():
     m.setCurrentPosition(0)
 
 # MOVE TO START POSITION
-m_top.startMotion(0, 20)
-m_bot.startMotion(0, -20)
-m_left.startMotion(0, -20)
-m_right.startMotion(0, 20)
+m_top.startMotion(0, 50)
+m_bot.startMotion(0, -50)
+m_left.startMotion(0, -50)
+m_right.startMotion(0, 50)
+
 
 def handle_sock(clientsock, addr):
     global PLAYING
@@ -90,11 +103,44 @@ def handle_sock(clientsock, addr):
         except:
             pass
 
+
+def det_acq():
+    # SHOULD BE IMPROVED!
+    # IT IS QUITE SLOW...
+    det_render = bge.texture.ImageRender(scene, det)
+    det_render.flip = True
+    # 512x256
+    width, height = det_render.size
+    det['im_number'] += 1
+    im_file = 'image-%03d.brw' % det['im_number']
+    det['im_file'] = im_file
+    det['width'] = width
+    det['height'] = height
+    a = np.asarray(det_render.image, dtype=np.uint8)
+    det['im_array'] = a.reshape((width, height, 4))
+    det['im_data'] = det['im_array'].tobytes()
+    with open(im_file, 'wb') as f:
+        f.write(det['im_data'])
+    # THIS FORMAT CAN BE LATER PROCESED BY:
+    # im_array = np.fromfile('filename')
+
+    # SAVE ALSO A SNAPSHOT
+    im = Image.frombytes('RGBA', (width, height), det['im_data'])
+    im.save(im_file.replace('brw', 'png'))
+
+    # SOME IDEAS TO PACK FOR FUTURE SOCKET TRANSFER
+    data = det['im_data']
+    # tdata = struct.pack("L", len(data))
+    # tdata = tdata + struct.pack("H", height)
+    # tdata = tdata + struct.pack("H", width)
+    # tdata = tdata + data
+
+
 def execute_cmd(cmd):
     global PLAYING, motors
     cmd_args = cmd.split()
     if cmd_args[0] in motor_names:
-        #<motor> <value>
+        # <motor> <value>
         m = motors[cmd_args[0]]
         cmd_v = float(cmd_args[1])
         p = m.getCurrentPosition()
@@ -106,14 +152,14 @@ def execute_cmd(cmd):
 
     if cmd.startswith(b'move'):
         pairs = cmd_args[1:]
-        for n,v in zip(pairs[::2], pairs[1::2]):
+        for n, v in zip(pairs[::2], pairs[1::2]):
             m = motors[n]
             cmd_v = float(v)
             p = m.getCurrentPosition()
             m.startMotion(p, cmd_v)
 
-    if cmd_args[0] in [b'acc', b'dec', b'vel']:
-        #<acc/vel> <motor> <value>
+    if cmd_args[0] in [b'acc', b'vel']:
+        # <acc/vel> <motor> <value>
         cmd_m = motors[cmd_args[1]]
         cmd_v = float(cmd_args[2])
 
@@ -122,16 +168,14 @@ def execute_cmd(cmd):
 
     if cmd.startswith(b'acc'):
         cmd_m.setAccelerationTime(cmd_v)
-
-    if cmd.startswith(b'dec'):
         cmd_m.setDecelerationTime(cmd_v)
 
-    if cmd_args[0] in [b'?pos', b'?state', b'?vel', b'?acc', b'?dec']:
+    if cmd_args[0] in [b'?pos', b'?state', b'?vel', b'?acc']:
         cmd_m = motors[cmd_args[1]]
         ans = cmd_args[0][1:].decode('utf-8')
-        ans += ' '+cmd_args[1].decode('utf-8')
+        ans += ' ' + cmd_args[1].decode('utf-8')
         if cmd_args[0] == b'?pos':
-            ans += ' '+str(cmd_m.getCurrentPosition())
+            ans += ' ' + str(cmd_m.getCurrentPosition())
             print(ans)
         if cmd_args[0] == b'?state':
             if cmd_m.isInMotion():
@@ -139,30 +183,38 @@ def execute_cmd(cmd):
             else:
                 ans += ' ON'
         if cmd_args[0] == b'?acc':
-            ans += ' '+str(cmd_m.getAccelerationTime())
-        if cmd_args[0] == b'?dec':
-            ans += ' '+str(cmd_m.getDecelerationTime())
+            ans += ' ' + str(cmd_m.getAccelerationTime())
         if cmd_args[0] == b'?vel':
-            ans += ' '+str(cmd_m.getMaxVelocity())
-        return ans+'\n'        
+            ans += ' ' + str(cmd_m.getMaxVelocity())
+        return ans + '\n'
 
     if cmd.startswith(b'?positions'):
         l = []
         for n in motor_names:
             l.append(str(motors[n].getCurrentPosition()))
         ans = ' '.join(l)
-        return ans+'\n'
+        return ans + '\n'
 
     if cmd.startswith(b'?states'):
-        ans = cmd_args[0][1:].decode('utf-8')
         states = []
         for n in motor_names:
             if motors[n].isInMotion():
                 states.append('MOVING')
             else:
                 states.append('ON')
-        ans += ' '.join(states)
-        return ans+'\n'
+        ans = ' '.join(states)
+        return ans + '\n'
+
+    if cmd.startswith(b'acq'):
+        det_acq()
+
+    if cmd.startswith(b'?acq_im_number'):
+        ans = 'acq_im_number %d' % det['im_number']
+        return ans + '\n'
+
+    if cmd.startswith(b'?acq_im_file'):
+        ans = 'acq_im_file %s' % det['im_file']
+        return ans + '\n'
 
 
 def handle_keyboard():
@@ -172,16 +224,17 @@ def handle_keyboard():
         try:
             bge.logic.NextFrame()
             if keyboard.events[bge.events.LEFTARROWKEY] != 0:
-                cam.applyMovement([-0.1,0,0], True)
+                cam.applyMovement([-0.1, 0, 0], True)
             elif keyboard.events[bge.events.RIGHTARROWKEY] != 0:
-                cam.applyMovement([0.1,0,0], True)
+                cam.applyMovement([0.1, 0, 0], True)
             elif keyboard.events[bge.events.UPARROWKEY] != 0:
-                cam.applyMovement([0,0.1,0], True)
+                cam.applyMovement([0, 0.1, 0], True)
             elif keyboard.events[bge.events.DOWNARROWKEY] != 0:
-                cam.applyMovement([0,-0.1,0], True)
+                cam.applyMovement([0, -0.1, 0], True)
             time.sleep(.01)
         except:
             pass
+
 
 def handle_mouse():
     global PLAYING
@@ -194,26 +247,29 @@ def handle_mouse():
             elif mouse.events[bge.events.WHEELDOWNMOUSE] != 0:
                 cam.lens -= 5
             elif mouse.events[bge.events.LEFTMOUSE] != 0:
-                cam.applyRotation([0,0.01,0], True)
+                cam.applyRotation([0, 0.01, 0], True)
             elif mouse.events[bge.events.RIGHTMOUSE] != 0:
-                cam.applyRotation([0,-0.01,0], True)        
+                cam.applyRotation([0, -0.01, 0], True)
             time.sleep(.01)
         except:
             pass
+
 
 def update_positions():
     global PLAYING, m_top, m_bot, m_left, m_right
     while PLAYING:
         try:
             bge.logic.NextFrame()
-            top.localPosition[2] = m_top.getCurrentPosition()/10.0
-            bot.localPosition[2] = m_bot.getCurrentPosition()/10.0
-            left.localPosition[0] = m_left.getCurrentPosition()/10.0
-            right.localPosition[0] = m_right.getCurrentPosition()/10.0
+            top.localPosition[2] = m_top.getCurrentPosition() / 10.0
+            bot.localPosition[2] = m_bot.getCurrentPosition() / 10.0
+            left.localPosition[0] = m_left.getCurrentPosition() / 10.0
+            right.localPosition[0] = m_right.getCurrentPosition() / 10.0
         except:
-            pass        
-    
-# CONFIGURE TCP SERVER
+            pass
+
+        # CONFIGURE TCP SERVER
+
+
 PLAYING = True
 serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -239,4 +295,4 @@ while PLAYING:
 serversock.shutdown(socket.SHUT_RDWR)
 serversock.close()
 
-#print('shutting down')
+# print('shutting down')
