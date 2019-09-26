b6b107407f5ab84300aebaad9fb8b8f2582d9e8e
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-09-26 16:13:56 +0200

    Implement detector acquisition in server

--- blender_slits/server.py
+++ blender_slits/server.py
@@ -17,8 +17,12 @@ along with Sardana-Training.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import bge
+import os
 import time
 import socket
+import logging
+import pathlib
+import itertools
 import numpy as np
 import h5py
 from PIL import Image
@@ -32,12 +36,125 @@ bot = scene.objects['b_bot']
 left = scene.objects['b_left']
 right = scene.objects['b_right']
 cam = scene.objects['Camera']
-det = scene.objects['Detector']
-det['im_number'] = 0
-det['im_file'] = 'No_file.brw'
-det['width'] = None
-det['height'] = None
-det['im_array'] = None
+
+
+class Acquisition:
+
+    def __init__(self, detector, exposure_time, nb_frames,
+                 saving_directory, image_format):
+        self.detector = detector
+        self.exposure_time = exposure_time
+        self.nb_frames = nb_frames
+        self.saving_directory = saving_directory
+        self.image_format = image_format
+        self.status = 'Ready'
+        self.start_time = None
+        self.end_time = None
+        self.task = None
+        self.stopped = False
+        self.images = []
+
+    def prepare(self):
+        if self.saving_directory:
+            os.makedirs(self.saving_directory, exist_ok=True)
+
+    def acquire(self):
+        self.detector.log.info(
+            'start acquisition nb_frames=%s exposure_time=%ss',
+            self.nb_frames, self.exposure_time)
+        try:
+            self._acquire()
+        finally:
+            self.status = 'Ready'
+        self.detector.log.info('Finished acquisition')
+
+    def _acquire(self):
+        self.start_time = time.time()
+        for frame in range(self.nb_frames):
+            if self.stopped:
+                break
+            self.status = 'Acquiring #{}'.format(frame)
+            det_render = self.detector.render()
+            end = time.time()
+            nap = self.exposure_time - (end - self.start_time)
+            if nap > 0:
+                time.sleep(nap)
+            self.status = 'Readout #{}'.format(frame)
+            # 512x256
+            width, height = det_render.size
+            image = det_render.image
+            a = np.asarray(image, dtype=np.uint8)
+            rgba_array = a.reshape((height, width, 4))
+            gray_array = rgb2gray(rgba_array)
+            im = Image.frombytes('RGBA', (width, height), rgba_array.tobytes())
+            if self.saving_directory and self.image_format:
+                self.status = 'Saving #{}'.format(frame)
+                image_nb = self.detector.next_image_number()
+                image_name = self.image_format.format(image_nb=image_nb)
+                image_path = pathlib.Path(self.saving_directory, image_name)
+                h5f = h5py.File(image_path.with_suffix('.h5'), "w")
+                h5f.create_dataset("img", data=gray_array.astype(np.uint8))
+                im.save(image_path.with_suffix('.png'))
+                self.detector.last_image_file_name = image_path.with_suffix('.h5')
+        self.end_time = time.time()
+        self.status = 'Ready'
+
+    def start(self):
+        if self.task is not None:
+            raise RuntimeError('Cannot start same acquisition twice')
+        self.task = Thread(target=self.acquire)
+        self.task.start()
+
+    def wait(self):
+        self.task.join()
+
+
+class Detector:
+
+    def __init__(self, scene, name):
+        self.scene = scene
+        self.name = name
+        self.exposure_time = 1.0
+        self.nb_frames = 1
+        self.image_counter = itertools.count()
+        self.acq = None
+        self.image_format = 'image-{image_nb:03d}'
+        self.last_image_file_name = ''
+        self.saving_directory = ''
+        self.log = logging.getLogger(name)
+
+    def render(self):
+        return bge.texture.ImageRender(self.scene, self.obj)
+
+    def next_image_number(self):
+        return next(self.image_counter)
+
+    @property
+    def obj(self):
+        return self.scene.objects[self.name]
+
+    @property
+    def acq_status(self):
+        return 'Ready' if self.acq is None else self.acq.status
+
+    def prepare_acquisition(self):
+        self.acq = Acquisition(self, self.exposure_time, self.nb_frames,
+                               self.saving_directory, self.image_format)
+        self.acq.prepare()
+
+    def start_acquisition(self):
+        if self.acq_status != 'Ready':
+            raise RuntimeError('Previous acquisition not finished yet!')
+        self.acq.start()
+
+    def stop_acquisition(self, wait=False):
+        acq = self.acq
+        if acq:
+            acq.stopped = True
+            if wait:
+                self.acq.wait()
+
+detector = Detector(scene, 'Detector')
 
 # WELL-ALIGNED (GAP 0) POSITIONS ARE:
 # top z=2   (4x2x1)+ROTX90
@@ -100,23 +217,20 @@ def handle_sock(clientsock, addr):
             pass
 
 
-def det_acq():
-    det_render = bge.texture.ImageRender(scene, det)
-    # 512x256
-    width, height = det_render.size
-    print("w: ", width, "h: ", height)
-    im_file = 'image-%03d.h5' % det['im_number']
-    a = np.asarray(det_render.image, dtype=np.uint8)
-    rgba_array = a.reshape((height, width, 4))
-    print("rgba_array.shape: ", rgba_array.shape)
-    gray_array = rgb2gray(rgba_array)
-    print("gray_array.shape: ", gray_array.shape)
-    print("gray_array_uint8.shape: ", gray_array.astype(np.uint8).shape)
-    h5f = h5py.File(im_file, "w")
-    h5f.create_dataset("img", data=gray_array.astype(np.uint8))
-    det['im_number'] = det['im_number'] + 1
-    im = Image.frombytes('RGBA', (width, height), rgba_array.tobytes())
-    im.save(im_file.replace('h5', 'png'))
+def prepare_acq():
+    try:
+        detector.prepare_acquisition()
+        return 'OK\n'
+    except Exception as err:
+        return 'ERROR: {}\n'.format(err)
+
+
+def start_acq():
+    try:
+        detector.start_acquisition()
+        return 'OK\n'
+    except Exception as err:
+        return 'ERROR: {}\n'.format(err)
 
 
 def execute_cmd(cmd):
@@ -188,16 +302,41 @@ def execute_cmd(cmd):
         ans = ' '.join(states)
         return ans + '\n'
 
-    if cmd.startswith(b'acq'):
-        det_acq()
+    if cmd.startswith(b'?acq_exposure_time'):
+        return 'acq_exposure_time {}\n'.format(detector.exposure_time)
 
-    if cmd.startswith(b'?acq_im_number'):
-        ans = 'acq_im_number %d' % det['im_number']
-        return ans + '\n'
+    if cmd.startswith(b'?acq_nb_frames'):
+        return 'acq_nb_frames {}\n'.format(detector.nb_frames)
 
-    if cmd.startswith(b'?acq_im_file'):
-        ans = 'acq_im_file %s' % det['im_file']
-        return ans + '\n'
+    if cmd.startswith(b'?acq_saving_directory'):
+        return 'acq_saving_directory {}\n'.format(detector.saving_directory)
+
+    if cmd.startswith(b'?acq_image_format'):
+        return 'acq_image_format {}\n'.format(detector.image_format)
+
+    if cmd.startswith(b'?acq_status'):
+        return 'acq_status {}\n'.format(detector.acq_status)
+
+    if cmd.startswith(b'?acq_last_image_file_name'):
+        return 'acq_last_image_file_name {}\n'.format(detector.last_image_file_name)
+
+    if cmd.startswith(b'acq_exposure_time'):
+        detector.exposure_time = float(cmd.split()[1])
+
+    if cmd.startswith(b'acq_nb_frames'):
+        detector.nb_frames = int(cmd.split()[1])
+
+    if cmd.startswith(b'acq_saving_directory'):
+        detector.saving_directory = cmd.split()[1].decode()
+
+    if cmd.startswith(b'acq_image_format'):
+        detector.image_format = cmd.split()[1].decode()
+
+    if cmd.startswith(b'acq_prepare'):
+        return prepare_acq()
+
+    if cmd.startswith(b'acq_start'):
+        return start_acq()
 
 
 def handle_keyboard():
@@ -253,6 +392,7 @@ def update_positions():
         # CONFIGURE TCP SERVER
 
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(levelname)-5s %(name)s: %(message)s')
 PLAYING = True
 serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
